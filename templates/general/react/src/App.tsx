87f6ffaaecdb62a2d27a71f373d41fff644f2484
Steve <stevedsimkins@gmail.com>
2025-04-17 16:41:55 -0400

    feat: Add general templates

---
+++ templates/general/react/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <img src="/logo.svg" alt="Orbiter logo" />
+      <a href="https://docs.orbiter.host" target='_blank' rel="noopener noreferrer" className='link'>Docs</a>
+    </main>
+  )
+}
+
+export default App
